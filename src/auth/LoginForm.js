1421a81228df45411ef0d159753f3f43fa512bd3
Caleb Matteis <matteismusic@gmail.com>
2021-05-17 07:42:32 -0400

    commit

--- src/auth/LoginForm.js
+++ src/auth/LoginForm.js
@@ -36,9 +36,54 @@ function LoginForm({ login }) {
       }
 
       return (
-          <div className="LoginForm">
-              
+        <div className="LoginForm">
+          <div className="container col-md-6 offset-md-3 col-lg-4 offset-lg-4">
+            <h3 className="mb-3">Log In</h3>
+  
+            <div className="card">
+              <div className="card-body">
+                <form onSubmit={handleSubmit}>
+                  <div className="form-group">
+                    <label>Username</label>
+                    <input
+                        name="username"
+                        className="form-control"
+                        value={formData.username}
+                        onChange={handleChange}
+                        autoComplete="username"
+                        required
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label>Password</label>
+                    <input
+                        type="password"
+                        name="password"
+                        className="form-control"
+                        value={formData.password}
+                        onChange={handleChange}
+                        autoComplete="current-password"
+                        required
+                    />
+                  </div>
+  
+                  {formErrors.length
+                      ? <Alert type="danger" messages={formErrors} />
+                      : null}
+  
+                  <button
+                      className="btn btn-primary float-right"
+                      onSubmit={handleSubmit}
+                  >
+                    Submit
+                  </button>
+                </form>
+              </div>
+            </div>
           </div>
-      )
+        </div>
+    );
     
 }
+
+export default LoginForm;
