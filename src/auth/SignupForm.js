ac6bb6495b268f2ac6ef08641e344771e11ba3a6
Caleb Matteis <matteismusic@gmail.com>
2021-05-18 03:50:12 -0400

    commit

--- src/auth/SignupForm.js
+++ src/auth/SignupForm.js
@@ -21,4 +21,90 @@ function SignupForm({ signup }) {
         "formData=", formData,
         "formErrors=", formErrors,
     );
+
+    // Handle form submit: Calls Login prop and, if succesful, redirect to /companies
+
+    async function handleSubmit(evt) {
+        evt.preventDefault();
+        let result = await signup(formData);
+        if (result.sucess) {
+            history.push("/companies");
+        } else {
+            setFormErrors(result.errors);
+        }
+    }
+
+    // Update form data field
+    function handleChange(evt) {
+        const { name, value } = evt.target;
+        setFormData(data => ({ ...data, [name]: value }));
+    }
+
+    return (
+        <div className="SignupForm">
+            <div className="container col-md-6 offset-md3 col-lg-4 offset-lg-4">
+                <h2 className="mb-3">Sign Up</h2>
+                <div className="card">
+                <div className="card-body">
+                <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                <label>Username</label>
+                <input
+                     name="username"
+                     className="form-control"
+                     value={formData.username}
+                     onChange={handleChange}
+                />
+                </div>
+                <div className="form-group">
+                <label>Password</label>
+                <input
+                   type="password"
+                   name="password"
+                   className="form-control"
+                   value={formData.password}
+                   onChange={handleChange}
+                />   
+                </div>
+
+                <div className="form-group">
+                <label>First Name</label>
+                <input
+                   name="firstName"
+                   className="form-control"
+                   value={formData.firstName}
+                   onChange={handleChange}
+                />
+                </div>
+                <div className="form-group">
+                <label>Email</label>
+                <input
+                   type="email"
+                   name="email"
+                   className="form-control"
+                   value={formData.email}
+                   onChange={handleChange}
+                />
+                </div>
+
+                {formErrors.length
+                  ?<Alert type="danger" messages={formErrors} />
+                  : null
+                }
+
+                <button
+                   type="submit"
+                   className="btn btn-primary float-right"
+                   onSubmit={handleSubmit}
+                >
+                 Submit
+                </button>
+                </form>
+                </div>
+                </div>
+            </div>
+        </div>
+    );
 }
+
+export default SignupForm;
