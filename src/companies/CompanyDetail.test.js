e94ef99991209ffb62c61baa57b829407289e47d
Caleb Matteis <matteismusic@gmail.com>
2021-05-19 05:12:05 -0400

    commit

--- src/companies/CompanyDetail.test.js
+++ src/companies/CompanyDetail.test.js
@@ -1,4 +1,27 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import { MemoryRouter, Route } from "react-router-dom";
-import { UserProvider } from ""
+import { UserProvider } from "../testUtils";
+
+it("renders without crashing", function () {
+    render(
+        <MemoryRouter>
+        <UserProvider>
+        <Company />
+        </UserProvider>
+        </MemoryRouter>
+    );
+});
+
+it("matches snapshot", function () {
+    const { asFragment } = render(
+        <MemoryRouter initialEntries={["/company/ibm"]}>
+        <UserProvider>
+        <Route path="/company/:handle">
+        <Company />
+        </Route>
+        </UserProvider>
+        </MemoryRouter>
+    );
+    expect(asFragment()).toMatchSnapshot();
+});
