ee2891adcfc342d370ca7cf6cb39c85abac98ec1
Caleb Matteis <matteismusic@gmail.com>
2021-05-20 06:49:20 -0400

    commit

--- src/homepage/HomePage.js
+++ src/homepage/HomePage.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-import "./Hompage.css";
+import "./Homepage.css";
 import UserContext from "../auth/UserContext";
 
 /** Homepage of site.
